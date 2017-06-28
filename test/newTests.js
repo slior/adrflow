eaa5226116fa2090562750481582ee7dbba34fe7
slior <lior.schejter@gmail.com>
2017-06-28 22:37:15 +0300

    removing some code duplication in test

--- test/newTests.js
+++ test/newTests.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const should = require('should')
 const rewire = require('rewire')
 
@@ -24,6 +26,19 @@ describe('New command',() => {
 
   let dummyLaunchEditor = (file,editorCmd) => {}
 
+  let commonMocks = {
+    fs : mockFS
+    , propUtil : mockPropUtil
+    , launchEditorFor : dummyLaunchEditor
+  }
+
+  function modifiedCommonMocks(specificMocks) {
+    let copy = {}
+    for (var k in commonMocks) copy[k] = commonMocks[k]
+    for (var k in specificMocks) copy[k] = specificMocks[k]
+    return copy;
+  }
+
   it("Should fail if passed an invalid title - that can't be used as a filename", () => {
     let revert = IC.__set__({
       findADRDir : (startFrom, callback,notFoundHandler) => { callback('.') }
@@ -41,29 +56,22 @@ describe('New command',() => {
   it ("Should assign the next number for the new ADR - one higher than the last available ADR", () => {
     let testTitle = "test"
 
-    var revert = IC.__set__({
+    var revert = IC.__set__(modifiedCommonMocks({
       findADRDir : mockFindADRDir
       , withAllADRFiles : (adrDir, callback) => { callback(['1-adr1.md','2-adr2.md'])}
       , adrContent : (num,title,date) => {
         num.should.eql(3) //one higher than 2-adr2.md
       }
-      , fs : mockFS
-      , propUtil : mockPropUtil
-      , launchEditorFor : dummyLaunchEditor
-    })
+    }))
 
     IC([testTitle])
 
-    revert = IC.__set__({
+    revert = IC.__set__(modifiedCommonMocks({
       findADRDir : mockFindADRDir
       , withAllADRFiles : (adrDir, callback) => { callback(['1-adr1.md','5-adr2.md'])}
       , adrContent : (num,title,date) => {
         num.should.eql(6) //one higher than 5-adr2.md
-      }
-      , fs : mockFS
-      , propUtil : mockPropUtil
-      , launchEditorFor : dummyLaunchEditor
-    })
+      }}))
 
     IC(["test"])
     revert();
@@ -71,32 +79,26 @@ describe('New command',() => {
 
   it("Should use the title given as title parts when creating the new ADR content", () => {
     let testTitle = "test"
-    var revert = IC.__set__({
+    var revert = IC.__set__(modifiedCommonMocks({
       findADRDir : mockFindADRDir
       , withAllADRFiles : (adrDir, callback) => { callback(['1-adr1.md'])}
       , adrContent : (num,title,date) => {
         title.should.eql(testTitle)
       }
-      , fs : mockFS
-      , propUtil : mockPropUtil
-      , launchEditorFor : dummyLaunchEditor
-    })
+    }))
 
     IC([testTitle])
     revert();
 
     let adrWithSeveralParts = ["adr","part","2"]
-    revert = IC.__set__({
+    revert = IC.__set__(modifiedCommonMocks({
       findADRDir : mockFindADRDir
       , withAllADRFiles : (adrDir, callback) => { callback(['1-adr1.md'])}
       , adrContent : (num,title,date) => {
         console.log(`Title mock got: ${title}`)
         title.should.eql(adrWithSeveralParts.join(' '))
       }
-      , fs : mockFS
-      , propUtil : mockPropUtil
-      , launchEditorFor : dummyLaunchEditor
-    })
+    }))
 
     IC(adrWithSeveralParts)
 
@@ -105,13 +107,11 @@ describe('New command',() => {
 
   it("should launch the editor configured in the adr configuration", () => {
 
-    let revert = IC.__set__({
+    let revert = IC.__set__(modifiedCommonMocks({
       launchEditorFor : (file,editorCmd) => {
         editorCmd.should.eql(mockEditorCommand)
       }
-      , fs : mockFS
-      , propUtil : mockPropUtil
-    })
+    }))
 
     IC(["testadr"])
 
