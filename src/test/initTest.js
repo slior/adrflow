4311c56264f97e71b12029a71752473fae0438bd
slior <lior.schejter@gmail.com>
2017-07-25 21:56:06 +0300

    code cleanup in initTests

--- src/test/initTest.js
+++ src/test/initTest.js
@@ -4,18 +4,24 @@ const rewire = require('rewire')
 
 const IC = rewire('../commands/init.js')
 
+let ensureDirMock = expected => {
+  return dir => {
+    dir.should.eql(expected)
+    return {
+      then : () => { return { catch : () => {} }}
+    }
+  }
+}
+
+let mockFS = expectedDir => {
+  return { ensureDir : ensureDirMock(expectedDir) }
+}
+
 describe('Init command',function() {
   it("should create the directory passed to it and issue a proper console log", () => {
     let testDir = "sample"
     let revert = IC.__set__({
-      fs : {
-        ensureDir : (dir) => {
-           dir.should.eql(testDir)
-           return {
-             then : () => { return { catch : () => {} }}
-           }
-        }
-      }
+      fs : mockFS(testDir)
       , console : {
         log : (msg) => { msg.should.eql("Created " + testDir)}
       }
@@ -29,14 +35,7 @@ describe('Init command',function() {
   it ("should default to default directory if no argument is passed", () => {
     let defaultDir = 'doc/adr'
     let revert = IC.__set__({
-      fs : {
-        ensureDir : (dir) => {
-           dir.should.eql(defaultDir)
-           return {
-             then : () => { return { catch : () => {} }}
-           }
-        }
-      }
+      fs : mockFS(defaultDir)
       , console : {
         log : (msg) => { msg.should.eql("Created " + defaultDir)}
       }
