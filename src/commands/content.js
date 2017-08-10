24dd2b69085052412894ccaf9deb80f1c7b2c80d
slior <lior.schejter@gmail.com>
2017-08-10 14:07:56 +0300

    changed 'content' command to not rely on adr_util

--- src/commands/content.js
+++ src/commands/content.js
@@ -1,17 +1,11 @@
 "use strict"
 
-let {withContentOf} = require('./adr_util.js')
+let adrContext = require('../adr_util_sync.js').createUtilContext()
 
 /**
  * Output the content of the given ID, if available.
  * @param {number} id - The ID of the ADR to output
  */
-let contentCmd = (id) => {
-    withContentOf(id
-                , content => {
-                    console.info(content)
-                }
-    )
-}
+let contentCmd = (id) => { console.info(adrContext.contentOf(id)) }
 
 module.exports = contentCmd
