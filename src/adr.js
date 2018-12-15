ad842df84b552a5b230b478d6ca09b857f482128
slior <lior.schejter@gmail.com>
2018-12-15 19:56:15 +0200

    Avoiding loading ('require') all the modules at startup

--- src/adr.js
+++ src/adr.js
@@ -2,67 +2,74 @@
 
 const program = require('commander')
 
-function loadCommand(cmd)
+function executeSingleParamCommand(cmd,param)
 {
-  if (cmd)
-  {
-    return require("./commands/" + cmd + ".js")
-  }
-  else throw new Error("Invalid command given" + cmd)
+    let c = require('./commands/' + cmd + ".js")
+    c(param)
 }
 
 //Setting up program commands
 
-program.version("0.1.0")
-
 program
   .description("ADR: Architecture Decision Records Utility")
-  .version('0.0.1')
+  .version('0.6.0')
   .usage("<command> args")
   .command("init [directory]")
     .description("Initialize ADR in the current or given directory")
-    .action(loadCommand('init'))
+    .action(_ => executeSingleParamCommand('init',_))
 
 program.command("new <title...>")
     .description("Create a new ADR with the given title")
-    .action(loadCommand('new'))
+    .action(titleParts => {
+        let cmd = require('./commands/new.js')
+        cmd(titleParts)
+    })
 
 program.command("accept <adrID>")
     .description("Accept the ADR with the given ID")
-    .action(loadCommand('accept'))
+    .action(_ => executeSingleParamCommand('accept',_))
 
 program.command("list")
     .description("List all current ADRs in this project")
     .option("-b --bare","list only bare filenames")
-    .action(loadCommand('list'))
+    .action(function(options) {
+        let cmd = require('./commands/list.js')
+        cmd(options)
+    })
   
 program.command("status <adrID>")
     .description("Return the status of the ADR designated by the given ID")
-    .action(loadCommand('status'))
+    .action(_ => executeSingleParamCommand('status',_))
 
 program.command("link <source> <link> <target>")
     .description("Link source ADR to target with the given text")
-    .action(loadCommand("link"))
+    .action((s,l,t) => {
+        let cmd = require('./commands/link.js')
+        cmd(s,l,t)
+    })
 
 program.command("export <id> [destinationFile]")
     .description("Export the given ADR to HTML format or to standard output if no file is given. Specify '*' as ID in order to export all. Specify a comma delimited list of IDs to export a subset of ADRs")
-    .action(loadCommand("export"))
+    .action((id,dest) => {
+        let cmd = require('./commands/export.js')
+        cmd(id,dest)
+    })
 
 program.command("content <id>")
     .description("Output the content of the ADR with the given id")
-    .action(loadCommand("content"))
+    .action(_ => executeSingleParamCommand("content",_))
 
 program.command("edit <id>")
     .description("Launch the configured editor for the given ADR")
-    .action(loadCommand("edit"))
+    .action(_ => executeSingleParamCommand("edit",_))
 
 program.command("search <term>")
     .description("Search the ADRs for the given term")
-    .action(loadCommand("search"))
+    .action(_ => executeSingleParamCommand("search",_))
 
 program.command("diagram [destinationFile]")
     .description("Create and output a network diagram (HTML-based) of the current ADRs")
-    .action(loadCommand("diagram"))
+    .action(_ => executeSingleParamCommand('diagram',_))
 
 process.on('uncaughtException', (err) => {
   console.error(`${err}`)
